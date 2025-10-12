b6f35407864e59054e4be0d1d89b306ef18b2420
parth120705 <munjalparth123@gmail.com>
2025-10-12 20:02:51 +0530

    readonly and optional

--- 02basics/objects.js
+++ 02basics/objects.js
@@ -1,13 +1,14 @@
-var User = {
-    name: "parth",
+"use strict";
+// const User={
+//     name:"parth",
+//     email:"p@gmail.com",
+//     isActive:true
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+var myUser = {
+    _id: "123",
+    name: "p",
     email: "p@gmail.com",
     isActive: true
 };
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
-}
-var newUser = { name: "parth", isPaid: true, email: "p@gmail.com" };
-createUser(newUser);
-function createCourse() {
-    return { name: "typescript", price: 500 };
-}
+myUser.name = "pm";
--- 02basics/objects.ts
+++ 02basics/objects.ts
@@ -1,30 +1,62 @@
-const User={
-    name:"parth",
-    email:"p@gmail.com",
-    isActive:true
+// const User={
+//     name:"parth",
+//     email:"p@gmail.com",
+//     isActive:true
+// }
+
+// // function createUser({name:string,isPaid:boolean}){}
+// // let newUser={name:"parth",isPaid:true,email:"p@gmail.com"}
+// // createUser(newUser);
+
+// function createCourse():{name:string,price:number}{
+//     return {name:"typescript",price:500};
+// }
+
+// // type aliases
+
+// type User={
+//     name:string,
+//     email:string,
+//     isActive:boolean
+// }
+
+// function createUser(user:User):User{
+//     return {name:"",email:"",isActive:true};
+// }
+// createUser({name:"",email:"",isActive:true});
+
+// //example use it as coordinates
+
+// type User={
+//     //readonly
+//     readonly _id:string,
+//     name:string,
+//     email:string,
+//     isActive:boolean,
+//     //optional
+//     credCardDetails?:number
+// }
+// let myUser:User={
+//     _id:"123",
+//     name:"p",
+//     email:"p@gmail.com",
+//     isActive:true
+// }
+// myUser.name="pm";
+// myUser._id="12";
+
+
+type cardNumber={
+    cardNumber:string
 }
 
-// function createUser({name:string,isPaid:boolean}){}
-// let newUser={name:"parth",isPaid:true,email:"p@gmail.com"}
-// createUser(newUser);
-
-function createCourse():{name:string,price:number}{
-    return {name:"typescript",price:500};
+type cardDate={
+    cardDate:string
 }
 
-// type aliases
-
-type User={
-    name:string,
-    email:string,
-    isActive:boolean
+type cardDetails=cardNumber&cardDate&{
+    cvv:number
 }
 
-function createUser(user:User):User{
-    return {name:"",email:"",isActive:true};
-}
-createUser({name:"",email:"",isActive:true});
-
-//example use it as coordinates
 
 export {}
