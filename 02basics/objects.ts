115393893c1374443b0f9e73b3fac99a23e7a760
parth120705 <munjalparth123@gmail.com>
2025-10-12 19:30:11 +0530

    use of type

--- 02basics/objects.ts
+++ 02basics/objects.ts
@@ -4,12 +4,27 @@ const User={
     isActive:true
 }
 
-function createUser({name:string,isPaid:boolean}){}
-let newUser={name:"parth",isPaid:true,email:"p@gmail.com"}
-createUser(newUser);
+// function createUser({name:string,isPaid:boolean}){}
+// let newUser={name:"parth",isPaid:true,email:"p@gmail.com"}
+// createUser(newUser);
 
 function createCourse():{name:string,price:number}{
     return {name:"typescript",price:500};
 }
+
+// type aliases
+
+type User={
+    name:string,
+    email:string,
+    isActive:boolean
+}
+
+function createUser(user:User):User{
+    return {name:"",email:"",isActive:true};
+}
+createUser({name:"",email:"",isActive:true});
+
+//example use it as coordinates
 
 export {}
